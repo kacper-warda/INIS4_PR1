ac00c3dfa325d134c587f99eb2477b29addaf768
Kacper Warda <kacper.warda@logintegra.com>
2021-04-21 09:27:25 +0200

    Zadanie 2

---
+++ src/com/company/Car.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Car {
+    public final String model;
+    public final String producer;
+    public String color;
+    public Double millage;
+    public Double engineVolume;
+
+    public Car(String model, String producer) {
+        this.model = model;
+        this.producer = producer;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,14 +7,22 @@ public class Main {
         Human me = new Human();
         me.firstName = "Kacper";
         me.lastName = "Warda";
-        me.pet = new Animal("Felis",2.0);
+        me.pet = new Animal("Felis", 2.0);
 
         System.out.println(me.firstName);
         System.out.println(me.pet.name);
 
-        me.pet = new Animal("Canis",13.0);
+        me.pet = new Animal("Canis", 13.0);
 
-        //jak dostać się do kota?
+        Car passat = new Car("Passat", "VW");
+        passat.color = "gray";
+        passat.millage = 80000.0;
+        passat.engineVolume = 1.9;
+
+        System.out.println("take auto: " + passat.producer + " " + passat.model);
+
+        me.car = passat;
+        System.out.println("mam auto: " + me.car.model);
 
     }
 }
